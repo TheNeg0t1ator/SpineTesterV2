c603f5fbab6d4a574bc15b1eb0f183ffd490076a
Kobe Dieryck <kobe.dieryck@icloud.com>
2025-08-05 18:04:31 +0200

    update, works with one using pio

--- Device/main.py
+++ Device/main.py
@@ -1,8 +1,38 @@
-import machine
-from hx711 import HX711 as fs
+from machine import Pin
+from hx711 import *
 
-hx = fs(machine.Pin(2), machine.Pin(3), 128)
+# 1. initalise the hx711 with pin 14 as clock pin, pin
+# 15 as data pin
+hx = hx711(Pin(4), Pin(5))
 
-hx2 = fs(machine.Pin(4), machine.Pin(5), 128)
-while True:
-    print("A",hx.read(), "B",hx2.read())
+# 2. power up
+hx.set_power(hx711.power.pwr_up)
+
+# 3. [OPTIONAL] set gain and save it to the hx711
+# chip by powering down then back up
+hx.set_gain(hx711.gain.gain_128)
+hx.set_power(hx711.power.pwr_down)
+hx711.wait_power_down()
+hx.set_power(hx711.power.pwr_up)
+
+# 4. wait for readings to settle
+hx711.wait_settle(hx711.rate.rate_10)
+
+# 5. read values
+
+while(True):# wait (block) until a value is read
+    val = hx.get_value()
+
+    # or use a timeout
+    if val := hx.get_value_timeout(250000):
+        # value was obtained within the timeout period
+        # in this case, within 250 milliseconds
+        print(val)
+
+    # or see if there's a value, but don't block if not
+    if val := hx.get_value_noblock():
+        print(val)
+
+# 6. stop communication with HX711
+hx.close()
+
